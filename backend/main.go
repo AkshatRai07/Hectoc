f3fad34b20c38adb995d44875fa9e4bd8d27b4f0
AkshatRai07 <sanamrai2007@gmail.com>
2026-01-15 23:57:35 +0530

    Init backend router

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import {
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+}
 
 func main() {
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+
 	fmt.Println("Hello, World!")
 }
